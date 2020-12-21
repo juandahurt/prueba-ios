896d110a7d1d472192bff5344ac5085c1489faa5
juandahurt <juandahurtadom@gmail.com>
2020-12-21 10:27:51 -0500

    Initial Commit

---
+++ prueba-ios/prueba_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_iosApp.swift
+//  prueba-ios
+//
+//  Created by juandahurt on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct prueba_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
